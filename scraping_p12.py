01f6073cbba7a36479a08e1752ae0fc768b0b213
migueling2015 <mprada.ing@gmail.com>
2020-05-01 23:44:56 -0500

    Structuring connectivity

--- scraping_p12.py
+++ scraping_p12.py
@@ -26,26 +26,14 @@ def scrap_page(url):
                 gears.connection_record('conneceteds_log.log',dict_content[key][0],'Connected --{}'.format(datetime.timestamp(datetime.now())))
                 s = BeautifulSoup(page_scraping.text, 'html.parser')
 
-    return dict_content
+            for section in dict_content:
+                print('\n{}'.format(section))
+                print('{}'.format(dict_content[section][0]))
+                dict_content[section].append(p12.extract_url_article(dict_content[section][0],'featured-article__container'))
+                dict_content[section].append(p12.extract_url_article(dict_content[section][0],'article-box__container'))
 
-    # print(p12.status_code)
-    # print(p12.text)
-    # print(p12.headers)
-    # print(p12.request.headers)
-    # print(p12.cookies)
-
-
+            return dict_content
 
 
-    # def extract_url_article(url_in,use_class):
-    #     p_temp=requests.get(url_in)
-    #     s_temp = BeautifulSoup(p_temp.text, 'html.parser')
-    #     array_container=s_temp.find_all('div',attrs={'class':use_class})
-    #     array_container_resulting=[i.find('h2').find('a').get('href') for i in array_container]
-    #     print(array_container_resulting)
 
-    # for section in dict_content:
-    #     print('\n{}'.format(section))
-    #     print('{}'.format(dict_content[section][0]))
-    #     extract_url_article(dict_content[section][0],'featured-article__container')
-    #     extract_url_article(dict_content[section][0],'article-box__container')
+
--- scraping_p12_gears.py
+++ scraping_p12_gears.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import functions_gears as gears
 
 def obtain_sections(s):
     arreglo=s.find('li',attrs={'class':'sections'}).find_all('li')
@@ -12,3 +13,14 @@ def obtain_sections(s):
         print('{} -- {} \n'.format(i.a.contents[0],linkSection))
         
     return dic_temp
+
+def extract_url_article(url_in,use_class):
+    p_temp=gears.stablish_connection(url_in)
+    try:
+        len(page_scraping)
+        gears.connection_record('errors_log.log',page_scraping[0],'noConnection --{}'.format(datetime.timestamp(datetime.now())))
+    except:
+        s_temp = BeautifulSoup(p_temp.text, 'html.parser')
+        array_container=s_temp.find_all('div',attrs={'class':use_class})
+        array_container_resulting=[i.find('h2').find('a').get('href') for i in array_container]
+    return array_container_resulting
